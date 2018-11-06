607bf429b499884e2b8a3732d6ab52ee8410c228
Brynn Shepherd <brynn.shepherd@gmail.com>
2018-11-06 15:05:27 -0500

    implemented curl module

--- bash.js
+++ bash.js
@@ -6,16 +6,21 @@ const curl = require('./curl.js');
 process.stdout.write('prompt > ');
 
 process.stdin.on('data', data => {
-  const cmd = data.toString().trim().split(" ");
+  const cmd = data
+    .toString()
+    .trim()
+    .split(' ');
   if (cmd[0] === 'pwd') {
     pwd();
   }
-
   if (cmd[0] === 'ls') {
     ls();
   }
   if (cmd[0] === 'cat') {
     cat(cmd[1]);
+  }
+  if (cmd[0] === 'curl') {
+    curl(cmd[1]);
   }
   process.stdout.write('\nprompt > ');
 });
--- curl.js
+++ curl.js
@@ -1,13 +1,11 @@
 const request = require('request');
-console.log(request);
 
-// module.exports = function(fileName) {
-//   fs.readFile(fileName, (err, data) => {
-//     if (err) {
-//       throw err;
-//     } else {
-//       process.stdout.write(data);
-//       // process.stdout.write('prompt >');
-//     }
-//   });
-// };
+module.exports = function(url) {
+  request(url, function(err, response, body) {
+    if (err) {
+      throw err;
+    } else {
+      process.stdout.write(body);
+    }
+  });
+};
