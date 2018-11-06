4544928d8048682da7f89fa558f05963fa29d791
Guli Kholmatova <gulikholmatova@gmail.com>
2018-11-06 14:49:16 -0500

    Second commit

--- bash.js
+++ bash.js
@@ -1,15 +1,21 @@
 const pwd = require('./pwd.js');
 const ls = require('./ls.js');
+const cat = require('./cat.js');
+const curl = require('./curl.js');
+
 process.stdout.write('prompt > ');
 
 process.stdin.on('data', data => {
-  const cmd = data.toString().trim();
-  if (cmd === 'pwd') {
+  const cmd = data.toString().trim().split(" ");
+  if (cmd[0] === 'pwd') {
     pwd();
   }
 
-  if (cmd === 'ls') {
+  if (cmd[0] === 'ls') {
     ls();
+  }
+  if (cmd[0] === 'cat') {
+    cat(cmd[1]);
   }
   process.stdout.write('\nprompt > ');
 });
