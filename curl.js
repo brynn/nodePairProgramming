c25ead9396462853f6cea85c4dc7e944a3aa3102
Guli Kholmatova <gulikholmatova@gmail.com>
2018-11-06 14:51:05 -0500

    Third message

---
+++ curl.js
@@ -0,0 +1,13 @@
+const request = require('request');
+console.log(request);
+
+// module.exports = function(fileName) {
+//   fs.readFile(fileName, (err, data) => {
+//     if (err) {
+//       throw err;
+//     } else {
+//       process.stdout.write(data);
+//       // process.stdout.write('prompt >');
+//     }
+//   });
+// };
